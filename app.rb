9817adfc423578e933b881a0e9f08c5733910cfb
Hailey Bongo <haileyebongo@gmail.com>
2020-04-18 17:57:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,12 +6,12 @@ class App < Sinatra::Base
     erb :index
   end
   
-  get '/create_puppy' do 
+  get '/new' do 
   
     erb :create_puppy
   end
   
-  post '/create_puppy' do 
+  post '/puppy' do 
     
     name = params[:name]
     age = params[:age]

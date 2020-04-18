145563f3ac10aacdd1fe6d8a534ba8a450e13411
Hailey Bongo <haileyebongo@gmail.com>
2020-04-18 17:55:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,10 @@ class App < Sinatra::Base
   
   get '/create_puppy'
     name = params[:name]
+    breed = params[:breed]
+    age = params[:age]
+    
+    @new_puppy = Puppy.new(name, breed, age)
     erb :create_puppy
   end
 

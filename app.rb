8a7f68cccf7121852138584def974a07b5ddc8ea
Hailey Bongo <haileyebongo@gmail.com>
2020-04-18 17:53:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :index
+  end
+  
+  get '/create_puppy'
+    name = params[:name]
+    erb :create_puppy
+  end
 
 end
